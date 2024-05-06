48723128709f21f2c10a8c121e2987a668b33a5a
CODEX1235 <147001058+CODEX1235@users.noreply.github.com>
2024-05-06 18:07:14 -0500

    Add files via upload

---
+++ Entregable.py
@@ -0,0 +1,26 @@
+
+
+
+user = "pp@gmail.com"
+key = 1234
+
+
+usuario = input("Bienvenido Usuario,ponga su usuario")
+clave = int(input("ponga la contraseña"))
+
+if  usuario == user :
+ print("el correo es correcto")
+
+elif usuario != user:
+ print("el correo es incorrecto")
+
+
+
+if key == clave :
+ print("la clave es correcta")
+
+elif key != clave:
+ print("la clave es incorrecta")
+
+else:
+ print("el usuario y la clave son incorrectos")
