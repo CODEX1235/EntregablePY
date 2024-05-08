bed08d0419a48c569fc4b87610d8b78c59bdab9b
CODEX1235 <147001058+CODEX1235@users.noreply.github.com>
2024-05-08 17:57:04 -0500

    Add files via upload

---
+++ entregable2/entregable2.py
@@ -0,0 +1,19 @@
+
+usuario = []
+usuarios = []
+
+while len(usuarios) < 5:
+ 
+    nombre = input("Inserte el Nombre")
+    usuario.append(nombre) 
+    apellido = input("Inserte el Apellido")
+    usuario.append(apellido)
+    telefono = input("Inserte el telefono")
+    usuario.append(telefono)
+    correo = input("inserte el correo")
+    usuario.append(telefono)
+    clave = input("inserte clave")
+    usuario.append(clave)
+    usuarios.append(usuario)
+
+print(usuarios)
